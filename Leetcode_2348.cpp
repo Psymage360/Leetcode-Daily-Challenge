9c32b2e94f5efcc7af47ad98b0200b30736cdcfd
Ayush Chaudhary <ayushchaudhary360@gmail.com>
2023-03-31 19:34:26 +0530

    Number of Zero-Filled Subarrays

---
+++ Leetcode_2348.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        long long ans = 0;
+        long long c = 0;
+        for(int i = 0;i<nums.size();i++){
+            if(nums[i]==0){
+                c++;
+            }
+            else{
+                ans+=(c*(c+1))/2;
+                c = 0;
+            }
+        }
+        if(c){
+            ans+=(c*(c+1))/2;
+            // c = 0;
+        }
+        return ans;
+    }
+};
